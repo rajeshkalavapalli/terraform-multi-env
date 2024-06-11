2e44c6a6d9877b7727aa69a39aa1b2b241237669
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-06-11 13:10:16 +0530

    multi env.tf

---
+++ dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "bigmatrix-state-dev"
+key    = "mul-dev"
+region = "us-east-1"
+dynamodb_table = "bigmatrix-locking-dev"
---
+++ prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "bigmatrix-state-prod"
+key    = "mul-prod"
+region = "us-east-1"
+dynamodb_table = "bigmatrix-locking-prod"
